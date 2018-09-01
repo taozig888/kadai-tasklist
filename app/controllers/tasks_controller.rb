547773c871b8c6423faac075ce1b25c975dc6cff
m.taka <taozig8@gmail.com>
2018-09-01 10:58:44 +0000

    twitter

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,23 +1,26 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  def index
-    @tasks = Task.all
-  end
-
-  def show
-  end
-
-  def new
-    @task = Task.new
-  end
+  before_action :correct_user, only: [:destroy]
+  
+ def new
+  @task = Task.new
+ end
+  
+ def index
+   if logged_in?
+     @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+   end
+ end
 
   def create
     @task = Task.new(task_params)
-
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
       redirect_to @task
     else
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
       flash.now[:danger] = 'Task が投稿されませんでした'
       render :new
     end
@@ -39,9 +42,8 @@ class TasksController < ApplicationController
 
   def destroy
     @task.destroy
-
     flash[:success] = 'Task は正常に削除されました'
-    redirect_to tasks_url
+    redirect_to root_url #tasks_url
   end
   
   private
@@ -54,4 +56,12 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:content, :status)
   end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
+  
 end
